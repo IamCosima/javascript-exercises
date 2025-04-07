8b33d855b66ff610f9d5e374dcd5ec800c044a17
IamCosima <uzairsarvan@gmail.com>
2025-04-07 11:19:22 +0200

    Finished Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string,repeatnum) {
+    repeating = string
 
+    if (repeatnum == 0){
+        return ""
+    }
+    else if (repeatnum <0){
+        return "ERROR"
+
+    }
+    for (let i = 1; i < repeatnum; i++) {
+        string = repeating + string;
+    }
+    return string
 };
 
 // Do not edit below this line
