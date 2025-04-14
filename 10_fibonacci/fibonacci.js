5ceb058ab45511da4bb5b301be6f8c537d5c2fff
IamCosima <uzairsarvan@gmail.com>
2025-04-14 14:13:47 +0200

    Finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(itterations) {
+    let count
+    if (typeof itterations !== 'number') {
+        count = parseInt(itterations)
+    } else {
+        count = itterations
+    }
+
+    if (count < 0) {
+        return "OOPS";
+    }else if (count == 0) {
+        return 0;
+    }else{
+        let fPrev = 1;
+        let sPrev = 0;
+    
+        for (let i = 2; i <= count; i++) {
+            let current = fPrev + sPrev;
+            sPrev = fPrev;
+            fPrev = current;
+        }
+        return fPrev;
+    }
 
 };
 
