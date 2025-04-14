aede22e24292331dec213d50533fd390beba6531
IamCosima <uzairsarvan@gmail.com>
2025-04-14 13:56:13 +0200

    Finished palindromes Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const allowed = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const stringlower = string.toLowerCase()
+
+    const stringsplit = stringlower.split('')
+
+    const stringfilter = stringsplit.filter((character) => allowed.includes(character)) 
+
+    const clean = stringfilter.join()
+
+    const reversedString = clean.split('').reverse().join('');
+
+    if(clean == reversedString) {
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
