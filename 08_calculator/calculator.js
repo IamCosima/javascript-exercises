2f5bdf3f8829442bd541f2fd8d5a5e07164de338
IamCosima <uzairsarvan@gmail.com>
2025-04-14 12:24:14 +0200

    Finished Sum,add,subtract,multipy functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,16 +1,30 @@
-const add = function() {
+const add = function(num1,num2) {
+  return num1 + num2
 	
 };
 
-const subtract = function() {
+const subtract = function(num1,num2) {
+	return num1 - num2
+};
+
+const sum = function(array) {
+  var sum = 0
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i]
+    
+  }
+  return sum
 	
 };
 
-const sum = function() {
+const multiply = function(array) {
+  var multiply = 1
+  for (let i = 0; i < array.length; i++) {
+    multiply = multiply * array[i]
+    
+  }
+  return multiply
 	
-};
-
-const multiply = function() {
 
 };
 
