ba45515b662c1583f66dda44facb745e376c2e9f
IamCosima <uzairsarvan@gmail.com>
2025-04-15 10:44:13 +0200

    Finished Get getTheTitles Function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let book = array.filter(array => array.title)
+    var books = [] 
+    console.log(book)
+    for (let i = 0; i < book.length; i++) {
+        books.push(array[i]["title"])
+        
+    }
+    return books
 };
 
 // Do not edit below this line
